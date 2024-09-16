b22769c2e445fb71d601c5b9160ed6064af56448
joaovmeyer <144701021+joaovmeyer@users.noreply.github.com>
2024-09-16 12:57:10 -0300

    Create main.py
    I coded this while I was tired and should be sleeping, so quite a bit of bad code and maybe my design choices were not the best, and it also doesn't work, so I still need to debug everything, but I feel I'm close to making it right

---
+++ main.py
@@ -0,0 +1,231 @@
+from random import choice;
+from math import sqrt, log;
+
+
+
+
+
+
+
+class Node:
+
+	def __init__(self, currentBoard, turn = -1):
+
+		self.board = copy2Darray(currentBoard);
+		self.possibleMoves = getPossibleMoves(self.board);
+		self.unvisitedNodes = copy2Darray(self.possibleMoves);
+		self.visitedNodes = [];
+		self.turn = turn;
+
+		self.isLeaf = len(self.unvisitedNodes) == 0;
+
+		self.visits = 1;
+		self.wins = 0;
+
+
+
+	def calculateUTC(self, parent):
+
+		# larger values of c lead to more exploratory agents
+		c = sqrt(2);
+
+		return (self.wins / self.visits) + c * sqrt(log(parent.visits) / self.visits);
+
+
+	def getBestUCT(self):
+
+		bestNode = None;
+		bestUCT = -1;
+
+		for node in self.visitedNodes:
+
+			UCT = node.calculateUTC(self);
+
+			if (UCT > bestUCT):
+				bestUCT = UCT;
+				bestNode = node;
+
+		return bestNode;
+
+	# select best node to explore
+	def selectNode(self):
+
+		if (self.isLeaf):
+			return self;
+
+		if (len(self.unvisitedNodes) == 0):
+			# find child with best UTC value
+			bestUTCChild = self.getBestUCT();
+
+			return bestUTCChild.selectNode();
+
+		return self.expand();
+
+
+	def makeNodeFromMove(self, move):
+		return Node(makeMoveOther(self.board, move, self.turn), -self.turn);
+
+	# removes a random unvisited child from the unvisited list (and adds it to the visited list) and returns it
+	def expand(self):
+		newNode = choice(self.unvisitedNodes);
+		self.unvisitedNodes.remove(newNode);
+
+		self.visitedNodes.append(self.makeNodeFromMove(newNode));
+
+		return self.visitedNodes[-1];
+
+
+	# make random moves untill game terminates, and go back up updating the nodes
+	def simulateGame(self):
+
+		if (self.isLeaf):
+			return getState(self.board) * self.turn;
+
+		randomMove = choice(self.possibleMoves);
+
+		result = self.makeNodeFromMove(randomMove).simulateGame();
+
+		self.visits += 1;
+		self.wins += result;
+
+		return result;
+
+
+	def bestChild(self):
+
+		bestChild = None;
+		mostVisits = -1;
+
+		for node in self.visitedNodes:
+			if (node.visits > mostVisits):
+				bestChild = node;
+				mostVisits = node.visits;
+
+		print("Oi:", bestChild.wins);
+		return bestChild;
+
+
+	def MCTS(self, n):
+		for i in range(n):
+			node = self.selectNode();
+			node.simulateGame();
+
+		return self.bestChild();
+
+
+
+
+
+
+
+
+def copy2Darray(array):
+	return [row[:] for row in array];
+
+
+def makeMoveOther(board, move, player):
+	newBoard = copy2Darray(board);
+	newBoard[move[0]][move[1]] = player;
+
+	return newBoard;
+
+def makeMove(board, move, player):
+	board[move[0]][move[1]] = player;
+
+def unmakeMove(board, move):
+	board[move[0]][move[1]] = 0;
+
+
+# this is the most terrible piece of code ever, but it should work
+# -1 is X wins, +1 if O wins, and 0 if neither of those
+def getState(board):
+	players = [-1, 1];
+
+	for i in range(2):
+
+		player = players[i];
+
+		isDiag1 = True;
+		isDiag2 = True;
+		isHorizontal = [True, True, True];
+		isVertical = [True, True, True];
+
+		for j in range(3):
+			isDiag1 = isDiag1 and (board[j][j] == player);
+			isDiag2 = isDiag2 and (board[j][2 - j] == player);
+
+			for k in range(3):
+				isHorizontal[j] = isHorizontal[j] and (board[j][k] == player);
+				isVertical[j] = isVertical[j] and (board[k][j] == player);
+
+		isWin = isDiag1 or isDiag2;
+		for j in range(3):
+			isWin = isWin or isHorizontal[j] or isVertical[j];
+
+		if (isWin):
+			return player;
+
+	return 0;
+
+
+def getPossibleMoves(board):
+	moves = [];
+
+	if (getState(board) != 0):
+		return moves;
+
+	for i in range(3):
+		for j in range(3):
+
+			# a move can be made only in an empty place
+			if (board[i][j] == 0):
+				moves.append((i, j));
+
+	return moves;
+
+
+def printBoard(board):
+
+	b = "";
+
+	for i in range(3):
+
+		for j in range(3):
+
+			if (board[i][j] == 0):
+				b += "   ";
+			elif (board[i][j] == -1):
+				b += " X ";
+			else:
+				b += " O ";
+
+			b += ("|" if j < 2 else "")
+
+		b += ("\n-----------\n" if i < 2 else "");
+
+	print(b);
+
+
+
+
+# 0 -> empty
+# -1 -> X
+# 1 -> O
+# initiate an empty board
+board = [
+	[0, 0, 0],
+	[0, 0, 0],
+	[0, 0, 0]
+];
+
+while True:
+
+	agent = Node(board, 1);
+	board = agent.MCTS(10).board;
+
+	printBoard(board);
+	move = list(map(int, input("\nYour move: ").split(",")));
+	makeMove(board, move, -1);
+
+
+
